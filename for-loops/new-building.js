9cb6c4ad68c139692bf3982d26e328d6a05b9826
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-19 23:47:15 +0100

    new building

---
+++ for-loops/new-building.js
@@ -0,0 +1,28 @@
+function solve(floor, rooms){
+
+    
+    let currentRoom = 0
+    let floorName = ''
+    let result = ``;
+
+    while ( floor >= 1){
+        if (floor == floor){
+            floorName == 'L';
+        }
+
+        for ( let i =0; i <= rooms-1; i ++){
+            currentRoom = i;
+            if (floor % 2 == 0){
+                floorName == 'O';
+            }else {
+                floorName == 'A';
+            }
+            
+        }
+        
+        
+
+    }
+    
+}
+solve(6, 4);
