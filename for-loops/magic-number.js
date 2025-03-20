961d32a9a8694ac21706ea0aad57e2dbb07c4138
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-21 00:06:28 +0100

    final setup

--- for-loops/magic-number.js
+++ for-loops/magic-number.js
@@ -2,25 +2,32 @@ function solve(start, end, magicNumber){
     let combinations = 0;
     let firstDigit = 0;
     let secondDigit = 0;
+    let sum = 0;
+    let condition = false;
 
     for ( let i = start; i <= end; i ++){
         firstDigit = i;
 
         for ( let j = start; j <= end; j++){
             secondDigit = j;
+            combinations += 1
 
-            if ( firstDigit + secondDigit === magicNumber){
-                combinations += 1
+            sum = firstDigit + secondDigit
+
+            if ( sum == magicNumber){
                 console.log(`Combination ${combinations} - (${firstDigit} + ${secondDigit} = ${magicNumber})`);
-                
-            }else {
-                console.log(`${combinations} combinations - neither equals ${magicNumber}`);
-                
+                condition = true;
+                break;
             }
+                
+            
+        if ( condition){
+            break
+        }else {
+            console.log(`${combinations} combinations - neither equals ${magicNumber}`);
         }
-        
+        }
     }
-    
 }
 solve(1, 10, 5)
-solve(23, 24, 20)
+//solve(23, 24, 20)
