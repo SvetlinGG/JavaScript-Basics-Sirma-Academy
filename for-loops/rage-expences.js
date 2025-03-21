a154ab628db54a85de0196e3edd9340e2fc34b1a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-22 00:02:36 +0100

    rage expences

--- for-loops/rage-expences.js
+++ for-loops/rage-expences.js
@@ -7,8 +7,25 @@ function solve(lostGames, headsetPrice, mousePrice, keyboardPrice, displayPrice)
     let totalTrashed = 0;
 
     for ( let i=1; i <= lostGames; i++){
-        
+        let currentGame = i;
+        if ( currentGame % 2 == 0){
+            trashedHeadset += 1;
+        }
+        if (currentGame % 3 == 0){
+            trashedMouse += 1;
+        }
+        if ( currentGame % 2 == 0 && currentGame % 3 == 0){
+            trashedKeyboard += 1;
+            if ( trashedKeyboard % 2 == 0){
+                trashedDisplay += 1;
+        } 
+        }
+            
     }
-
+    totalTrashed = trashedHeadset*headsetPrice + trashedMouse*mousePrice + trashedKeyboard*keyboardPrice + trashedDisplay*displayPrice;
+    console.log(`Rage expenses: ${totalTrashed.toFixed(2)} lv."`);
+    
+    
 }
-solve()
+solve(7, 2, 3, 4, 5);
+solve(23, 12.5, 21.5, 40, 200);
