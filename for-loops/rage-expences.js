1235934231312455b884c3ef127abaff8f56a59f
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-21 23:42:09 +0100

    rage

---
+++ for-loops/rage-expences.js
@@ -0,0 +1,14 @@
+function solve(lostGames, headsetPrice, mousePrice, keyboardPrice, displayPrice){
+
+    let trashedHeadset = 0;
+    let trashedMouse = 0;
+    let trashedKeyboard = 0;
+    let trashedDisplay = 0;
+    let totalTrashed = 0;
+
+    for ( let i=1; i <= lostGames; i++){
+        
+    }
+
+}
+solve()
