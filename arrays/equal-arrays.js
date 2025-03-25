5bcb35e8d0b58bbfb2467888cd722af48e6509bb
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-25 11:50:22 +0100

    equal arrays

--- arrays/equal-arrays.js
+++ arrays/equal-arrays.js
@@ -1,9 +1,32 @@
-function solve(){
+function solve(arr1, arr2){
 
-    const array1 = [1, "a", new Date("09 March 2025 15:38:00 UTC")];
-const localeString = array1.toLocaleString("en", { timeZone: "UTC" });
+    let numArr1 = arr1.map(num => Number(num));
+    let numArr2 = arr2.map(num => Number(num));
+    let sum1 = 0;
+    let sum2 = 0;
+    let indexI = 0;
+    
 
-console.log(localeString);
+    for ( let i = 0; i < numArr1.length; i ++){
+        indexI = i;
+        sum1 += numArr1[i];
+        sum2 += numArr2[i];
+        if ( numArr1[i] !== numArr2[i]){
+            console.log(`Arrays are not identical. Found difference at ${i} index`);
+        }
+    };
+    if ( sum1 === sum2){
+        console.log(`Arrays are identical. Sum: ${sum1}`);
+        
+    }
+    
+    
+    
+
+    
+    
 
 }
-solve();
+solve(['10','20','30'], ['10','20','30']);
+solve(['1','2','3','4','5'], ['1','2','4','4','5']);
+solve(['1'], ['10'])
