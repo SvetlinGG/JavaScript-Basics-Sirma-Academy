ee1b08cfb44a2ec37dcf6ea7c91fabb57bb89ca3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-26 23:41:49 +0100

    array Manipulation

--- arrays/array-manipulation.js
+++ arrays/array-manipulation.js
@@ -1,30 +1,26 @@
 function solve(arr){
 
-    let newArr = [];
-    newArr.push(arr[0]);
-    
-    for ( let i = 1; i < arr.length; i++){
-        if ( arr[i] === 'Add 3'){
-            newArr.push(3);
-        }else if ( arr[i] === 'Remove 2'){
-            newArr.slice(1, 3);
-        }else if ( arr[i] === 'RemoveAt 1'){
-            newArr.splice(arr[i], 0);
-        }else if (arr[i] === 'insert 8 3'){
-            newArr.splice(arr[i], 0, 8)
-        }
+    let newArr = arr[0].split(', ').map(Number);
+    for ( let i =1; i < arr.length; i++){
+        [operation, first, second] = arr[i].split(' ');
+        first = Number(first);
+        second = Number(second);
+
+
+         if ( operation === 'Add'){
+            newArr.push(first);
+         }else if ( operation === 'Remove'){
+            newArr = newArr.filter(num => num !== first);
+         }else if ( operation === 'RemoveAt'){
+            newArr.splice(first, 1)
+         }else if( operation === 'Insert'){
+            newArr.splice(second, 0, first)
+         }
+            
         
     }
-    console.log(newArr);
+    console.log(newArr.join(' '));
     
-    
-    
-    
-    
-    
-    
-
-
 }
 solve(["4, 19, 2, 53, 6, 43",
     "Add 3",
