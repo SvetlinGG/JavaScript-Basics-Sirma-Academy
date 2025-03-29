08ac5c4ea407883b86bfd335019e00c77358c0b9
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-29 14:45:40 +0100

    person-data

---
+++ objects/person-data.js
@@ -0,0 +1,14 @@
+function person(firstName, lastName, age){
+
+    let personData = {
+        firstName,
+        lastName,
+        age
+    };
+
+    console.log(personData);
+    
+
+
+}
+person('Ivan', 'Ivanov', 29);
