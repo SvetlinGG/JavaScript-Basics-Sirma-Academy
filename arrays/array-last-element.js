513fed3cf50a866471e943c89be2ab3b8013820e
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-04 23:13:04 +0200

    Add files via upload

---
+++ arrays/array-last-element.js
@@ -0,0 +1,12 @@
+// Array.prototype.last = function() {
+//     return this.length ? this[this.length - 1] : -1;
+//   };
+//   const nums = [1, 2, 3];
+//   console.log(nums.last()); // 3
+  
+//   const empty = [];
+//   console.log(empty.last()); // -1
+    
+
+const uniqueNumbers = new Set([1, 2, 2, 3, 4, 4]);
+console.log(uniqueNumbers);
